4f5e7dd6b26d16755c8d6a41352d132738762226
Kartikeya Vats <kartikeya@Kartikeyas-MacBook-Pro.local>
2022-12-08 21:44:05 -0500

    updated code for DSPipeline

--- src/create_metrics.py
+++ src/create_metrics.py
@@ -5,45 +5,43 @@ import json
 import glob
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import shutil
 
 
 config_path = "../config/config.json"
 cfg = json.load(open(config_path,'r'))
 test_path = cfg['data_dir']['test']
+infer_model_path = cfg['infer_params']['infer_model_path']
 test_img = []
 test_imgs = []
 y_gt = []
+img_names = []
 folders = glob.glob(test_path + '/' + '*')
+i = 0
 for folder in folders:
     imgs = glob.glob(folder + '/' + '*')
     for img in imgs:
+        i+=1
         gt_folder = folder.split('/')[-1]
         if gt_folder=='PNEUMONIA':
             y_gt.append(1)
         else:
             y_gt.append(0)
         test_img = cv2.imread(img)
-        test_img_resized = cv2.resize(test_img, (224,224))
+        img_names.append(img)
+        test_img_resized = cv2.resize(test_img, (299,299)) 
         test_imgs.append(test_img_resized)
 
 
 test_imgs = np.array(test_imgs)
 test_imgs = test_imgs/255.0
 
+model = keras.models.load_model(infer_model_path)
 
-# test_data=test_datagen.flow_from_directory(self.test_path,
-#                                                 target_size=(self.image_shape,self.image_shape),
-#                                                 batch_size=self.batch_size,
-#                                                 class_mode='categorical')
 
-model = keras.models.load_model('../../Dataset/Models/baseline_resnet_chest_xray_e10_b64.h5')
 y_prediction = model.predict(test_imgs)
+y_probas = y_prediction
 y_prediction = np.argmax(y_prediction, axis=1).tolist()
-
-print(y_prediction)
-input("y_prediction")
-
-print(y_gt)
 
 
 result = confusion_matrix(y_gt, y_prediction)
@@ -54,3 +52,11 @@ print(tp,fp,tn,fn)
 
 print("precision is:", (tp)/(tp+fp))
 print("recall is:", (tp)/(tp+fn))
+
+print(result)
+
+
+
+
+
+
